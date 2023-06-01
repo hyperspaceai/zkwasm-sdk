7f4ed7b3ce614aee79c69aad64e61379a696e146
Kartik Nair <kartikajitnair@gmail.com>
2023-06-01 18:20:21 +0400

    Testing `JSModule`

--- src/zkwasm.js
+++ src/zkwasm.js
@@ -84,7 +84,7 @@ export class Module {
 
   /**
    * Initializes the module (preparing it for export invocations).
-   * @returns Promise
+   * @returns {Promise}
    */
   async init() {
     if (this.initialized()) {
@@ -102,7 +102,6 @@ export class Module {
     this.worker = await initWorker();
 
     this.worker.postMessage({ action: "init_module", args: [this.binary] });
-    console.log(this.worker, "posted message");
 
     return new Promise((res) => {
       this.worker.addEventListener("message", (e) => {
@@ -173,6 +172,93 @@ export class Module {
         }
       });
     });
+  }
+}
+
+export class JSModule {
+  /**
+   * Creates a new uninitialized `JSModule` object.
+   * @param {string} source
+   */
+  constructor(source) {
+    this._module = null;
+    this.source = source;
+  }
+
+  /**
+   * Initializes the module (preparing it for function invocations).
+   * @returns {Promise}
+   */
+  async init() {
+    if (this.initialized()) {
+      throw "JSModule double initialization";
+    }
+
+    let exec_js_binary = [
+      0, 97, 115, 109, 1, 0, 0, 0, 1, 8, 1, 96, 3, 127, 127, 127, 1, 127, 2, 29,
+      2, 3, 101, 110, 118, 6, 109, 101, 109, 111, 114, 121, 2, 0, 1, 3, 101,
+      110, 118, 7, 101, 120, 101, 99, 95, 106, 115, 0, 0, 3, 2, 1, 0, 7, 11, 1,
+      7, 101, 120, 101, 99, 95, 106, 115, 0, 1, 10, 13, 1, 11, 0, 32, 0, 32, 1,
+      32, 2, 16, 0, 15, 11, 0, 34, 4, 110, 97, 109, 101, 1, 27, 2, 0, 7, 101,
+      120, 101, 99, 95, 106, 115, 1, 15, 101, 120, 101, 99, 95, 106, 115, 95,
+      119, 114, 97, 112, 112, 101, 114,
+    ];
+    this._module = await Module.fromBinary(new Uint8Array(exec_js_binary));
+  }
+
+  /**
+   * Creates and initializes a `JSModule` instance.
+   * @param {string} source
+   * @returns {Promise<JSModule>}
+   */
+  static async fromSource(source) {
+    let mod = new JSModule(source);
+    await mod.init();
+    return mod;
+  }
+
+  /**
+   * Returns if this `JSModule` object has been intialized or not.
+   * @returns {boolean}
+   */
+  initialized() {
+    return this._module !== null;
+  }
+
+  /**
+   * Call a function within the JS module with your provided arguments.
+   * @param {string} functionName
+   * @param {Uint8Array[]} args
+   * @returns {Promise<InvocationResult>}
+   */
+  async call(functionName, args) {
+    if (!this.initialized()) {
+      throw "Attempt to use uninitialized JS module.";
+    }
+
+    let allArgsLen = args
+      .map((arr) => arr.byteLength)
+      .reduce((acc, a) => acc + a, 0);
+    let mergedArgs = new Uint8Array(allArgsLen + args.length * 4);
+    let dataView = new DataView(mergedArgs.buffer);
+
+    let i = 0;
+    args.forEach((arr) => {
+      dataView.setUint32(i, arr.byteLength, true);
+      i += 4;
+      for (let j = 0; j < arr.byteLength; j++) {
+        mergedArgs[i + j] = arr[j];
+      }
+      i += arr.byteLength;
+    });
+
+    console.log(args, mergedArgs);
+
+    return await this._module.invokeExport("exec_js", [
+      new TextEncoder().encode(this.source),
+      new TextEncoder().encode(functionName),
+      mergedArgs,
+    ]);
   }
 }
 
