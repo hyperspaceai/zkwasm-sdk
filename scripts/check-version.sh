58248ba59f45f4cb6b4d379bc650531af5ab69ec
DaveHyperspace <dave@hyperspace.us>
2023-06-01 17:09:28 +0100

    Chore: check version before publishing

---
+++ scripts/check-version.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Exit if any subcommand fails
+set -e 
+
+# Get the version from package.json
+CURRENT_VERSION=$(node -p "require('./package.json').version")
+
+# Fetch updates from the remote repository
+git fetch
+
+# Get the version from package.json on the main branch
+MASTER_VERSION=$(git show origin/main:package.json | node -p "JSON.parse(require('fs').readFileSync(0)).version")
+
+# If the versions are not equal, then it has been updated
+if [ "$CURRENT_VERSION" != "$MASTER_VERSION" ]; then
+  echo "Version has been updated."
+  exit 0
+else
+  echo "Version has not been updated. Please update version before publishing."
+  exit 1
+fi
